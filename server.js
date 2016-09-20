1c710eb47fb300bd8b9a32963881927548f2615c
Justin Igawa <justinigawa13@yahoo.com>
2016-09-19 19:26:33 -0700

    changed content

--- server.js
+++ server.js
@@ -1,26 +1,31 @@
-//lets require/import the http module
+//Lets require/import the HTTP module
 var http = require('http');
 
-//lets define a port we want to listen to
-var PORT = 7000;
-var PORT2 = 7500;
+//Lets define a port we want to listen to
+var PORT1=7000;
+var PORT2=7050;
 
 //We need a function which handles requests and send response
-function handleRequest(request, response){
-	response.end("You are Awesome!");
-	response.end("You suck!");
+function handleRequest1(request, response){
+    response.end('You are awesome~ ' + request.url);
+}
+//We need a function which handles requests and send response
+function handleRequest2(request, response){
+    response.end('You suck~ ' + request.url);
 }
 
-//create a server
-var server = http.createServer(handleRequest);
+//Create a server
+var server1 = http.createServer(handleRequest1);
+//Create a server
+var server2 = http.createServer(handleRequest2);
 
 //Lets start our server
-server.listen(PORT, function(){
-	//callback triggered with server is successfully listening, Hurray!
-	console.log("Server listening on: http://localhose:%s", PORT);
+server1.listen(PORT1, function(){
+    //Callback triggered when server is successfully listening. Hurray!
+    console.log("Server listening on: http://localhost:%s", PORT1);
 });
 
-server.listen(PORT2, function(){
-	//callback triggered with server is successfully listening, Hurray!
-	console.log("Server listening on: http://localhose:%s", PORT2);
+server2.listen(PORT2, function(){
+    //Callback triggered when server is successfully listening. Hurray!
+    console.log("Server listening on: http://localhost:%s", PORT2);
 });
