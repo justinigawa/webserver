de8dccda1fd0f2c704ed74665d1df2ce09a83fb0
Justin Igawa <justinigawa13@yahoo.com>
2016-09-19 19:21:58 -0700

    changed files

--- server.js
+++ server.js
@@ -7,7 +7,8 @@ var PORT2 = 7500;
 
 //We need a function which handles requests and send response
 function handleRequest(request, response){
-	response.end('It Works!! Path Hit: ' + request.url);
+	response.end("You are Awesome!");
+	response.end("You suck!");
 }
 
 //create a server
@@ -17,11 +18,9 @@ var server = http.createServer(handleRequest);
 server.listen(PORT, function(){
 	//callback triggered with server is successfully listening, Hurray!
 	console.log("Server listening on: http://localhose:%s", PORT);
-	console.log("You're Awesome!");
 });
 
 server.listen(PORT2, function(){
 	//callback triggered with server is successfully listening, Hurray!
 	console.log("Server listening on: http://localhose:%s", PORT2);
-	console.log("You suck!");
 });
